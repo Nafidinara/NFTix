888a85d001a6b2d79637d3951c534dad9a42fbde
Teresa Tan <tanteresa36@gmail.com>
2024-11-22 16:35:00 +0800

    added testing contract

---
+++ test/ConcertTicketSystem.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import {Test, console} from "forge-std/Test.sol";
+import {Counter} from "../src/Counter.sol";
+import {VotingSystem} from "../src/ConcertTicketSystem.sol";
